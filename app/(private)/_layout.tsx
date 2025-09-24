83b2623b07253c951e3a6c0a182759f731b6de80
Uzochukwu Victor <vickzuzo@gmail.com>
2025-09-24 08:35:06 +0100

    fix: lefthook commitlint installation

--- app/(private)/_layout.tsx
+++ app/(private)/_layout.tsx
@@ -1,12 +1,10 @@
-
 import { Stack } from 'expo-router';
 
 export default function PrivateLayout() {
-    return (
-        <Stack screenOptions={{ headerShown: false }}>
-            <Stack.Screen name="home" />
-            <Stack.Screen name="qr-code" />
-        </Stack>
-
-    );
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="home" />
+      <Stack.Screen name="qr-code" />
+    </Stack>
+  );
 }
--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,14 @@
 /* eslint-env node */
-const { defineConfig } = require('eslint/config');
+const { defineConfig, globalIgnores } = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
+const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
 
 module.exports = defineConfig([
+  globalIgnores(['dist/*']),
   expoConfig,
+  eslintPluginPrettierRecommended,
   {
-    ignores: ['dist/*'],
-  },
-  {
+    files: ['babel.config.js'],
     rules: {
       'react/display-name': 'off',
     },
