6f30d99efeb09cfe5133b58ebb2f874be7cee09d
kenicodex <kehindesalaudeen222@gmail.com>
2025-09-25 13:19:10 +0100

    chore: add chore name to branch prefix

--- .lefthook/pre-commit/branch_checker.sh
+++ .lefthook/pre-commit/branch_checker.sh
@@ -4,7 +4,7 @@
 branch_name=$(git rev-parse --abbrev-ref HEAD)
 
 # Define the branch naming convention regex (change as needed)
-regex="^(fix|feat|int|integration|feature|bugfix|hotfix)\/[A-Za-z0-9\-]+$"
+regex="^(chore|fix|feat|int|integration|feature|bugfix|hotfix)\/[A-Za-z0-9\-]+$"
 
 # Check if the branch name matches the naming convention
 if [[ $branch_name =~ $regex ]]; then
@@ -12,6 +12,6 @@ if [[ $branch_name =~ $regex ]]; then
   exit 0
 else
   echo "ERROR: Branch name '$branch_name' does NOT follow the naming convention."
-  echo "Branch names should start with 'fix/', 'feat/', 'int/', 'integration/', 'feature/', 'bugfix/', or 'hotfix/' followed by a name."
+  echo "Branch names should start with 'chore/', 'fix/', 'feat/', 'int/', 'integration/', 'feature/', 'bugfix/', or 'hotfix/' followed by a name."
   exit 1
 fi
